f3838c3f40e8cf851cd2820efe57813cd828756e
AndreyBMWX6 <a_s_bykov@mail.ru>
2020-11-28 04:09:37 +0300

    Deleted extra comments in logging

--- internal/node/node.go
+++ internal/node/node.go
@@ -114,7 +114,6 @@ func (n *RaftCore) TryRecvClientMsg() message.ClientMessage {
 	for {
 		select {
 		case msg := <-n.ClientIn:
-			log.Println("got msg from client")
 			return msg
 		default:
 			return nil
